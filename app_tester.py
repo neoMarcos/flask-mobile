39ac9a303c4b5e6a0bd498e32afee404d2399c0c
neoMarcos <nedokoro@rediffmail.com>
2020-07-23 00:08:12 -0300

    Update app_tester.py
    Testing in Heroku console

--- app_tester.py
+++ app_tester.py
@@ -2,4 +2,14 @@ import unittest
 
 from app import app
 
-class BasicTest(unittest.TestCase):
+class BasicTestCase(unittest.TestCase):
+
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'Hello, World!')
+
+
+if __name__ == '__main__':
+    unittest.main()
